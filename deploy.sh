0bed60965ef1cb6157ab476dfb2e5a797a055f27
Arda Tugay <ardactugay@gmail.com>
2017-10-04 15:41:23 -0400

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+JEKYLL_ENV=production bundle exec jekyll build
+cp CNAME _site/
+git stash
+git checkout -b dev-ready
+sed -i.gitignore '/_site\//d' .gitignore
+rm -rf .gitignore.gitignore
+git add .gitignore _site/
+git commit -m "Deploying new version"
+git subtree split --prefix _site -b deployment
+git branch -f master deployment
+git checkout master
+git push -f origin master
+git branch -D dev-ready deployment
+git checkout development
+git stash pop
