6f7b69fd39b39a8cb1b635991e8e2a8934e13855
Arda Tugay <ardactugay@gmail.com>
2017-10-08 15:19:07 -0400

    Logo text is now clickable

---
+++ test/components/navbar/navbar.js
@@ -0,0 +1,7 @@
+module.exports = function() {
+    describe('Navbar', function() {
+        before(function() {
+            casper.start('http://localhost:4000/');
+        });
+    });
+};
--- test/run.js
+++ test/run.js
@@ -1,3 +1,18 @@
 var skillsPage = require('./pages/skills/skills');
 
-skillsPage();
+var navbar = require('./components/navbar/navbar');
+
+casper.options.onResourceRequested = function(casper, requestData, request) {
+    var skip = [
+        'www.googletagmanager.com'
+    ];
+
+    skip.forEach(function(needle) {
+        if (requestData.url.indexOf(needle) > 0) {
+            request.abort();
+        }
+    });
+};
+
+// skillsPage();
+navbar();
